7162a980cece613e6b58762128011b7369760381
Larissa Leal <larissa.iurk@gmail.com>
2021-12-02 09:13:56 -0300

    feat: update index

--- stories/Thing.stories.tsx
+++ stories/Thing.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+import { Thing, Props } from '../src/Thing';
 
 const meta: Meta = {
   title: 'Welcome',
